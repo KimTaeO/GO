dd273eb377c98e2238ee1adc62873e8cd90cde09
KimTaeO <taeo0328@gmail.com>
2023-09-20 08:34:32 +0900

    간단한 예제로 테스트코드 짜기

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
-package main
+package hello
 
-import "fmt"
+import (
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
+func Say(names []string) string {
+	if len(names) == 0 {
+		names = []string{"world"}
+	}
+	return "Hello, " + strings.Join(names, ", ") + "!"
 }
