62d0180aeb852d37840af928138dc2bfbb6fb99d
KimTaeO <taeo0328@gmail.com>
2023-09-24 01:51:20 +0900

    인터페이스 타입 변환 예제 구현

--- interface/interface.go
+++ interface/interface.go
@@ -8,15 +8,30 @@ type User struct {
 	age         int
 }
 
+type Teacher struct {
+	name string
+	age  int
+}
+
 // 위와 같이 인터페이스를 선언 가능하다
 // 메서드명(파라미터) 반환타입 형식으로 선언 가능하다
 // 인터페이스의 기본값은 nil이다
 type Abstract interface {
-	updateName(user *User, name string)
+	updateName(name string)
 }
 
-func (u *User) updateName(user *User, name string) {
-	user.name = name
+func (t *Teacher) updateName(name string) {
+	t.name = name
+}
+
+func (u *User) updateName(name string) {
+	u.name = name
+}
+
+func updateName(a Abstract, name string) {
+	if a != nil {
+		a.updateName(name)
+	}
 }
 
 // 1. 메서드는 반드시 메서드명이 있어야 한다.
@@ -31,7 +46,7 @@ func main() {
 
 	fmt.Println(u)
 
-	u.updateName(&u, "kotlin")
+	updateName(&u, "golang")
 
 	fmt.Println(u)
 }
